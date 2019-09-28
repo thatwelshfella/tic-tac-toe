a5f4a91573b8dd2a4d31e70f188549860af4d9bc
Luke Delaney <delaney.luke95@gmail.com>
2019-09-28 02:11:03 +0100

    fontAwesome added

---
+++ src/fontAwesome.js
@@ -0,0 +1,5 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faCircle, far } from "@fortawesome/free-regular-svg-icons";
+
+export default library.add(faTimes, faCircle, far);
